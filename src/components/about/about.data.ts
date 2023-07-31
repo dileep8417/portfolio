a618d56eb297af1e05477bfde9d514be77cff612
dileep <dileep8417@gmail.com>
2023-07-31 23:34:46 +0530

    Added resume

--- src/components/about/about.data.ts
+++ src/components/about/about.data.ts
@@ -4,6 +4,6 @@ export const lastName: string = 'Atyam';
 
 export const description: string = 'Working for a product-based company where I transform ideas into reality, making a meaningful impact on millions of users';
 
-export const resumeLink: string = '';
+export const resumeLink: string = '/DileepDeveloperResume.pdf';
 
 export const githubLink: string = 'https://github.com/dileep8417';
