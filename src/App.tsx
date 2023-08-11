bdb8bd82ef2f563d2a37f1d28717809bcd65c669
dileep <dileep8417@gmail.com>
2023-08-11 23:02:30 +0530

    Added Page tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SplashScreen from './components/SplashScreen';
 import Content from './components/Content';
 import './styles/App.css';
 import AppContext from './AppContext';
+import axios from 'axios';
 
 const App = function() {
+
+    useEffect(() => {
+        const trackPage = async () => {
+            const pageTrackURL = 'https://page-tracker.onrender.com/track';
+            await axios.post(pageTrackURL);
+        }
+
+        trackPage();
+    }, []);
+
     return (
         <AppContext>
             <div className='page-container'>
