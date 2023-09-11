26ec9ded2126617b614b8a5131f56dead1b12281
dileep <dileep8417@gmail.com>
2023-09-11 23:06:05 +0530

    Updated projects and resume

--- src/components/projects/projects.data.ts
+++ src/components/projects/projects.data.ts
@@ -1,4 +1,6 @@
 import resumeBuilderImg from '../../assets/images/projects/resume-builder.png';
+import browserImg from '../../assets/images/projects/movie-browser.png';
+import designerImg from '../../assets/images/projects/designer.png';
 import recipeImg from '../../assets/images/projects/recipe.png';
 import mailTrackerImg from '../../assets/images/projects/mail-tracker.png';
 import shopperImg from '../../assets/images/projects/shopper.png';
@@ -16,6 +18,28 @@ export const projects = [
         theme: '#272838'
     },
     {
+        title: 'Designer Portfolio',
+        description: 'A responsive & modern portfolio developed for designer to help in promoting the offers and services provide.',
+        workLinks: {
+            sourceCode: 'https://github.com/dileep8417/designer_portfolio',
+            demo: 'https://gopichand.design/'
+        },
+        image: designerImg,
+        features: ['Responsive', 'Modern'],
+        theme: 'rgb(64 64 64)'
+    },
+    {
+        title: 'Movie Browser',
+        description: 'A progressive web application developed using React, Redux Toolkit, and webpack to let the users browse the details of latest movies.',
+        workLinks: {
+            sourceCode: 'https://github.com/dileep8417/GSIV23_Dileep_Atyam',
+            demo: 'https://movie-browser-pwa.netlify.app/'
+        },
+        image: browserImg,
+        features: ['Infinite scrolling', 'Movie Search', 'Detailed Information'],
+        theme: 'rgb(104 110 255)'
+    },
+    {
         title: 'Recipe Finder',
         description: 'An immersive and user-friendly React application designed to help users discover a diverse range of delectable recipes effortlessly.',
         workLinks: {
@@ -23,7 +47,7 @@ export const projects = [
             demo: 'https://dileep8417.github.io/my_recipes/'
         },
         image: recipeImg,
-        features: ['Recipe Search', 'Add to Favourite', 'Detailed information'],
+        features: ['Recipe Search', 'Add to Favourite', 'Cooking Details'],
         theme: '#7E7F9A'
     },
     {
